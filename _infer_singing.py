0d1e1f195cf9ae12fc15acbe8c6cca47609af32c
annn <annnmusic37@gmail.com>
2024-08-15 14:59:36 +0800

    smoothing & iterative infer

--- _infer_singing.py
+++ _infer_singing.py
@@ -1,44 +1,55 @@
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+os.environ['CUDA_VISIBLE_DEVICES'] = '6'
 from infer import main
 
-model_path = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/singing-55M-20240713-03:40/model/epoch-75-step-250000-loss-3.872418055152893/model.pt'
-config_path = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/config/singing-55M.json'
-exp = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/singing-55M-20240713-03:40'
-resample = None
-dnsmos = True
-
-# model_path = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/singing-55M-encoder-loss-20240721-13:54/model/epoch-30-step-200000-loss-3.9031185120105745/model.pt'
-# config_path = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/config/singing-55M-encoder-loss.json'
-# exp = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/singing-55M-encoder-loss-20240721-13:54/'
+# model_path = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/20240805-02:28-singing-120M/model/epoch-31-step-65000-loss-4.3617/model.pt'
+# config_path = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/config/singing-120M.json'
+# exp = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/20240805-02:28-singing-120M'
 # resample = None
 # dnsmos = True
 
+# # simulated data
+# input_folder = '/mnt/data2/zhangjunan/enhancement/data/singing_scp/testset_unseen/noisy'
 
-# simulated data
-input_folder = '/mnt/data2/zhangjunan/enhancement/data/singing_scp/testset_unseen/noisy'
+# main(model_path, config_path, input_folder, exp, resample, dnsmos)
 
-main(model_path, config_path, input_folder, exp, resample, dnsmos)
+# model_path = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/20240729-12:57-singing-55M/model/epoch-272-step-600000-loss-3.5587/model.pt'
+# config_path = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/config/singing-55M.json'
+# exp = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/20240729-12:57-singing-55M'
+# resample = None
+# dnsmos = True
+# # simulated data
+# input_folder = '/mnt/data2/zhangjunan/enhancement/data/singing_scp/testset_unseen/noisy'
 
-model_path = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/singing-55M-encoder-loss-20240722-19:04/model/epoch-6-step-250000-loss-3.8247188554286957/model.pt'
-config_path = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/config/singing-55M-encoder-loss.json'
-exp = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/singing-55M-encoder-loss-20240722-19:04'
-resample = None
-dnsmos = True
-# simulated data
-input_folder = '/mnt/data2/zhangjunan/enhancement/data/singing_scp/testset_unseen/noisy'
+# main(model_path, config_path, input_folder, exp, resample, dnsmos)
 
-main(model_path, config_path, input_folder, exp, resample, dnsmos)
-
-# # real data
+# real data
 # input_folders = [
-#     '/mnt/data2/zhangjunan/enhancement/data/huawei/processed/v1劣化/noisy',
-#     '/mnt/data2/zhangjunan/enhancement/data/huawei/processed/v2劣化/noisy',
-#     '/mnt/data2/zhangjunan/enhancement/data/huawei/processed/v3多混响劣化/noisy',
-#     '/mnt/data2/zhangjunan/enhancement/data/huawei/processed/加混再分离劣化/noisy',
-#     '/mnt/data2/zhangjunan/enhancement/data/huawei/processed/真实室内环境数据/noisy',
-#     '/mnt/data2/zhangjunan/enhancement/data/huawei/processed/真实演唱会数据/noisy'
+#     '/mnt/data2/zhangjunan/enhancement/data/singing_scp/testset_unseen/noisy',
+#     # '/mnt/data2/zhangjunan/enhancement/data/huawei/processed/v1劣化/noisy',
+#     # '/mnt/data2/zhangjunan/enhancement/data/huawei/processed/v2劣化/noisy',
+#     # '/mnt/data2/zhangjunan/enhancement/data/huawei/processed/v3多混响劣化/noisy',
+#     # '/mnt/data2/zhangjunan/enhancement/data/huawei/processed/加混再分离劣化/noisy',
+#     # '/mnt/data2/zhangjunan/enhancement/data/huawei/processed/真实室内环境数据/noisy',
+#     # '/mnt/data2/zhangjunan/enhancement/data/huawei/processed/真实演唱会数据/noisy'
 # ]
+# model_path = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/20240809-20:24-singing-120M/model/epoch-199-step-400000-loss-3.7716/model.pt'
+# config_path = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/config/singing-120M.json'
+# exp = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/20240809-20:24-singing-120M'
+# resample = None
+# dnsmos = True
 
 # for input_folder in input_folders:
 #     main(model_path, config_path, input_folder, exp, resample, dnsmos)
+
+
+
+input_folder = '/mnt/data2/zhangjunan/dns2020-repo/datasets/test_set/real_recordings'
+
+model_path = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/20240812-23:07-urgentchallenge2024-77M/model/epoch-106-step-170000-loss-4.845/model.pt'
+config_path = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/config/urgentchallenge2024-77M.json'
+exp = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/20240812-23:07-urgentchallenge2024-77M'
+resample = None
+dnsmos = True
+
+main(model_path, config_path, input_folder, exp, resample, dnsmos)
--- infer_iterative.py
+++ infer_iterative.py
@@ -49,48 +49,6 @@ def load_model(model_path, config, device):
     model.eval()
     return model
 
-def smooth_audio_transition(audio_chunks, overlap=1024):
-    """
-    使用重叠相加法（overlap-add）拼接音频段，并且通过加大overlap并在拼接前进行能量平滑来减少clip声。
-    
-    参数:
-    audio_chunks (list of torch.Tensor): 需要拼接的音频段列表，每个段都是一个 torch 张量。
-    overlap (int): 用于平滑处理的重叠部分的长度，默认为1024。
-
-    返回:
-    torch.Tensor: 拼接后的完整音频。
-    """
-    if len(audio_chunks) == 0:
-        return torch.Tensor([])
-
-    # Hanning 窗口，用于平滑拼接的重叠区域
-    window = torch.hann_window(overlap * 2, periodic=True).to(audio_chunks[0].device)
-
-    # 初始化拼接结果
-    result = audio_chunks[0]
-    
-    for i in range(1, len(audio_chunks)):
-        # 获取前一个片段的末尾部分
-        previous_end = result[:, -overlap:].clone()
-        
-        # 上一个段的末尾应用 Hanning 窗口
-        previous_end *= window[overlap:]
-        
-        # 当前段的开头应用 Hanning 窗口
-        current_start = audio_chunks[i][:, :overlap].clone()
-        current_start *= window[:overlap]
-
-        # 将上一个片段的末尾和当前片段的开头叠加
-        transition = previous_end + current_start
-        
-        # 拼接过渡区到结果中
-        result[:, -overlap:] = transition
-        
-        # 将当前段的剩余部分拼接到结果中，保留音频长度
-        result = torch.cat((result, audio_chunks[i][:, overlap:]), dim=1)
-
-    return result
-
 def infer(model, input_folder, output_folder, device):
     os.makedirs(output_folder, exist_ok=True)
     enhanced_dir = os.path.join(output_folder, 'enhanced')
@@ -101,7 +59,7 @@ def infer(model, input_folder, output_folder, device):
     file_list = [f for f in os.listdir(input_folder) if f.endswith('.wav') or f.endswith('.flac') or f.endswith('.mp3')]
     assert len(file_list) > 0, f"No audio files found in {input_folder}."
     
-    window_size = 512 * model.seq_len
+    window_size = 512 * 256  # 窗口大小
 
     for file_name in tqdm(file_list):
         file_path = os.path.join(input_folder, file_name)
@@ -135,8 +93,7 @@ def infer(model, input_folder, output_folder, device):
             start += window_size  # 移动窗口，这里不重叠
 
         # 将所有处理过的音频片段拼接起来
-        # enhanced_signal = torch.cat(enhanced_audio, dim=1)
-        enhanced_signal = smooth_audio_transition(enhanced_audio, overlap=1024)
+        enhanced_signal = torch.cat(enhanced_audio, dim=1)
 
         # 剪裁到原始音频的长度
         enhanced_signal = enhanced_signal[:, :original_length]
@@ -183,15 +140,70 @@ def main(model_path, config_path, input_folder, exp, resample=None, dnsmos=False
         calculate_dnsmos_score(final_output_folder, './evaluation/DNSMOS', csv_path=os.path.join(output_folder, 'dnsmos.csv'))
 
 if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description='Generate audio outputs from a given folder of audio files.')
-    parser.add_argument('--exp', type=str, required=True, help='Path to the experiment folder.')
-    parser.add_argument('--model', type=str, required=True, help='Path to the trained model file.')
-    parser.add_argument('--config', type=str, required=True, help='Path to the model configuration file.')
-    parser.add_argument('--input_folder', type=str, required=True, help='Folder containing input audio files.')
-    parser.add_argument('--resample', type=int, default=None, help='Resample input audio to this rate before inference.')
-    parser.add_argument('--dnsmos', action='store_true', help='Compute DNSMOS scores for the output audio.')
+    # import argparse
+    # parser = argparse.ArgumentParser(description='Generate audio outputs from a given folder of audio files.')
+    # parser.add_argument('--exp', type=str, required=True, help='Path to the experiment folder.')
+    # parser.add_argument('--model', type=str, required=True, help='Path to the trained model file.')
+    # parser.add_argument('--config', type=str, required=True, help='Path to the model configuration file.')
+    # parser.add_argument('--input_folder', type=str, required=True, help='Folder containing input audio files.')
+    # parser.add_argument('--resample', type=int, default=None, help='Resample input audio to this rate before inference.')
+    # parser.add_argument('--dnsmos', action='store_true', help='Compute DNSMOS scores for the output audio.')
     
-    args = parser.parse_args()
+    # args = parser.parse_args()
     
-    main(args.model, args.config, args.input_folder, args.exp, args.resample, args.dnsmos)
+    # main(args.model, args.config, args.input_folder, args.exp, args.resample, args.dnsmos)
+    os.environ['CUDA_VISIBLE_DEVICES'] = '4'
+    model_path = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/masksr-55M-encoder-loss-20240719-17:07/model/epoch-24-step-400000-loss-4.812219809150696/model.pt'
+    config_path = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/config/masksr-55M-encoder-loss.json'
+    config = json5.load(open(config_path))
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    
+    model = load_model(model_path, config, device)
+    output_folder = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/_iterative_sample'
+    os.makedirs(output_folder, exist_ok=True)
+    enhanced_dir = os.path.join(output_folder, 'enhanced')
+    noisy_dir = os.path.join(output_folder, 'noisy')
+    os.makedirs(enhanced_dir, exist_ok=True)
+    os.makedirs(noisy_dir, exist_ok=True)
+
+    input_folder = '/mnt/data2/zhangjunan/muse-maskgit-pytorch/exp/_iterative_sample/data'
+    file_list = [
+        'clnsp102_traffic_248091_3_snr0_tl-21_fileid_268.wav',
+        # '59_simulated.flac'
+    ]
+    assert len(file_list) > 0, f"No audio files found in {input_folder}."
+    
+    window_size = 512 * 256  # 窗口大小
+
+    for file_name in tqdm(file_list):
+        file_path = os.path.join(input_folder, file_name)
+        signal, sr = torchaudio.load(file_path)
+        signal = torch.mean(signal, dim=0, keepdim=True)  # 如果音频有多个声道，取平均值
+        signal = signal.to(device)
+
+        # 重采样到模型需要的采样率
+        resampler = torchaudio.transforms.Resample(orig_freq=sr, new_freq=44100).to(device)
+        signal = resampler(signal)
+
+        original_length = signal.shape[-1]  # 保存原始音频的长度
+
+        # 用于存储处理过的音频片段
+        enhanced_audio = []
+        start = 0
+        end = start + window_size
+        segment = signal[:, start:end]
+        
+        if segment.shape[-1] < window_size:
+            segment = torch.nn.functional.pad(segment, (0, window_size - segment.shape[-1]))
+        
+        with torch.no_grad():
+            # 假设model.generate返回增强的音频
+            ids_by_timestep, audios_by_timestep = model.generate(segment.unsqueeze(0), timesteps=40, return_audios_every_timestep=True)
+            # enhanced_audio.append(output_segment.squeeze(0))
+
+        for i in range(len(audios_by_timestep)):
+            output_file_path = os.path.join(enhanced_dir, f'{file_name.split(".")[0]}_timestep_{i}.wav')
+            torchaudio.save(output_file_path, audios_by_timestep[i].squeeze(0).detach().cpu(), 44100)
+        
+        torchaudio.save(os.path.join(noisy_dir, file_name), segment.detach().cpu(), 44100)
+        print(f"Processed {file_name} -> {output_file_path}")
